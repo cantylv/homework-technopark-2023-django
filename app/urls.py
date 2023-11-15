f9b9f6deb962def9202594fd3c5bfd4f1d125621
Lobanov Ivan <physic2003@mail.ru>
2023-11-15 14:04:10 +0300

    move /static into folder /app and add some routing

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listing),
-    path('user/auth', views.authorization),
-    path('user/reg', views.registration),
+    path('/', views.listing),  # listing questions
+    path('/hot/', views.question),  # hot questions
+    path('/news/', views.listing),  # new questions
+    path('/tag/django/', views.tag),  # tag questions
+    path('/question/35/', views.question),  # question and answers
+    path('/profile/', views.profile),  # user profile
+    path('/login/', views.authorization),  # login
+    path('/signup/', views.registration),  # registration
+    path('/ask/', views.registration),  # ask question
 ]
--- app/views.py
+++ app/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def listing(req):
+    questions = []
+    return render(req, 'app/public/user_unauthorized/listing_unauthorized.html')
+
+
+def question(req):
+    return render(req, 'app/public/user_unauthorized/question_unauthorized.html')
+
+
+def profile(req):
+    return render(req, 'app/public/user_unauthorized/tag_questions_unauthorized.html')
+
+
+def tag(req):
+    return render(req, 'app/public/user_unauthorized/tag_questions_unauthorized.html')
+
+
 def authorization(req):
     return render(req, 'app/public/authorization.html')
 
 
 def registration(req):
     return render(req, 'app/public/registration.html')
-
-
-def listing(req):
-    return render(req, 'app/public/user_unauthorized/listing_unauthorized.html')
-
-

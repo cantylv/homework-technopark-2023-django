92df725a1a3bb6e9d3b0ef3836beb34cb0ed84a0
Lobanov Ivan <physic2003@mail.ru>
2023-11-16 16:23:04 +0300

    finished ask.html auth.html listing.html reg.hmtl | add right layout in the page | change routes in app/urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listing),  # listing questions
-    path('hot/', views.question),  # hot questions
-    path('news/', views.listing),  # new questions
-    path('tag/django/', views.tag),  # tag questions
-    path('question/35/', views.question),  # question and answers
-    path('profile/', views.profile),  # user profile
-    path('login/', views.authorization),  # login
-    path('signup/', views.registration),  # registration
-    path('ask/', views.ask),  # ask question
+    path('', views.listing, name='home'),  # listing questions    WELL DONE
+    path('hot/', views.question, name='hot_quest'),  # hot questions
+    path('news/', views.listing, name='new_quest'),  # new questions
+    path('tag/django/', views.tag, name='tag_url'),  # tag questions
+    path('question/35/', views.question, name='question_url'),  # question and answers
+    path('user/profile/', views.profile, name='profile'),  # user profile
+    path('user/auth/', views.authorization, name='auth'),  # login
+    path('user/reg/', views.registration, name='reg'),  # registration
+    path('user/ask/', views.ask, name='ask'),  # ask question   WELL DONE
 ]
